c209887630a44a4f057476b0598497bb9458cf86
lwg1103 <lwg1103@wp.pl>
2021-12-30 09:52:51 +0100

    init commit

---
+++ src/test/java/service/cardtrainer/handcalculator/HandcalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package service.cardtrainer.handcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandcalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
